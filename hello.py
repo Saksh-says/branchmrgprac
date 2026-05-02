1d8a1f74de20ec85f5d185000dee418b59ff6461
UserSaksh <saksh@gmail.com>
2026-05-02 06:36:10 +0530

    the switch se bani branch

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello branchmrgprac")
 print("Not dev1 and dev2 sitted together and resolved the hello.py")
 print("Again done some hardships by dev 1 in the branch")
+print("used $ git switch -c branch_name to easily create branch ")
